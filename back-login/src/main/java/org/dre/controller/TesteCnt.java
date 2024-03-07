73c0d6f0f809434a01a5ecaec986aeb6cf115aeb
dregh1 <dregh@gmail.com>
2024-03-07 11:36:46 +0300

    modif

---
+++ back-login/src/main/java/org/dre/controller/TesteCnt.java
@@ -0,0 +1,66 @@
+package org.dre.controller;
+
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+@Path("/teste")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class TesteCnt {
+
+    @Inject
+    JsonWebToken jwt;
+
+    @GET()
+    @Path("permit-all")
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@Context SecurityContext ctx) {
+        return getResponseString(ctx);
+    }
+//hello + anonymous, isHttps: false, authScheme: null, hasJWT: false
+
+
+    ///
+    @GET
+    @Path("roles-allowed")
+    @RolesAllowed({ "User", "Admin" })
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloRolesAllowed(@Context SecurityContext ctx) {
+        return getResponseString(ctx) + ", birthdate: " + jwt.getClaim("birthdate").toString();
+    }
+
+    ////////////////////
+    private String getResponseString(SecurityContext ctx) {
+        String name;
+        if (ctx.getUserPrincipal() == null) {
+            name = "anonymous";
+        } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
+            throw new InternalServerErrorException("Principal and JsonWebToken names do not match");
+        } else {
+            name = ctx.getUserPrincipal().getName();
+        }
+        return String.format("hello + %s,"
+                        + " isHttps: %s,"
+                        + " authScheme: %s,"
+                        + " hasJWT: %s",
+                name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+    }
+
+    private boolean hasJwt() {
+        return jwt.getClaimNames() != null;
+    }
+    //////////////////////////////////////////////////////////////
+    //Configuration de l'information sur la sécurité de l'extension de SmallRye JWT
+    //////////////////////////////////////////////////////////////
+
+
+
+
+}
